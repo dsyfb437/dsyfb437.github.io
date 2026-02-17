54ac92b6c7f5f3112ed230dde16d332e4f58c223
dsyfb437 <dsyfb_437@outlook.com>
2026-02-17 23:48:32 +0800

    Site updated: 2026-02-17 23:48:27

---
+++ js/random-bg.js
@@ -0,0 +1,13 @@
+// 随机首页背景图
+document.addEventListener('DOMContentLoaded', function() {
+  var banner = document.querySelector('#banner');
+  if (banner) {
+    var bgImages = [
+      '/img/index/e18620860ca7c7aa6b95c1a3f7a2d591.jpg',
+      '/img/index/微信图片_20260208215222_32811_5.jpg',
+      '/img/index/微信图片_20260217234400_34539_5.jpg'
+    ];
+    var randomBg = bgImages[Math.floor(Math.random() * bgImages.length)];
+    banner.style.backgroundImage = 'url(' + randomBg + ')';
+  }
+});
